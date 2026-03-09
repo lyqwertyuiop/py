179def1f14dcda382f906becbf23f37192978a6a
yang luo <258996510@qq.com>
2026-03-09 23:03:21 +0800

    2.0

--- 分数计算.py
+++ 分数计算.py
@@ -1,5 +1,7 @@
 # 分数计算.py
 
+#导入
+
 #公倍数捆绑包
 #最大公约数
 def gcd(m, n):
@@ -18,13 +20,13 @@ def anl(mom,long_,now,num_now):
     return anl(mom,long_,now+1,num_now)
 
 #通分
-def tong(son, mom):
-        long_num = len(son)
-        all_num_lcm = anl(mom,long_num,0,1)
+def tong(son1, mom1,son2, mom2):
+    num = lcm(mom1, mom2)
+    son1 *= num // mom1
+    son2 *= num // mom2
+    one_fen = [son1, son2, num]
+    return one_fen
         
-        for i in range(long_num):
-            mom[i] = all_num_lcm
-            son[i] = son[i] * (all_num_lcm // mom[i])
 
 #约分
 def yue(son, mom):
@@ -39,35 +41,25 @@ class know:
     def __init__(self):
         pass
     
-    def add(self, son, mom):
-        tong(son, mom)
-        sum_son = 0
-        for i in range(len(son)):
-            sum_son += son[i]
-        return yue(sum_son, mom[0])
-
-    def sub(self, son, mom):
-        tong(son, mom)
-        sum_son = 0
-        for i in range(len(son)):
-            sum_son -= son[i]
-        return yue(sum_son, mom[0])
-
-    def mul(self, son, mom):
-        sum_son = 1
-        sum_mom = 1
-        for i in range(len(son)):
-            sum_son *= son[i]
-            sum_mom *= mom[i]
-        return yue(sum_son, sum_mom)
-
-    def div(self, son, mom):
-        sum_son = 1
-        sum_mom = 1
-        for i in range(len(son)):
-            sum_son *= son[i]
-            sum_mom *= mom[i]
-        return yue(sum_son, sum_mom)
+    def add(self, son1, mom1, son2, mom2):
+        now_fen = tong(son1, mom1, son2, mom2)
+        new_fen = [now_fen[0]+now_fen[1], now_fen[2]]
+        return yue(new_fen[0], new_fen[1])
+    
+    def sub(self, son1, mom1, son2, mom2):
+        now_fen = tong(son1, mom1, son2, mom2)
+        new_fen = [now_fen[0]-now_fen[1], now_fen[2]]
+        return yue(new_fen[0], new_fen[1])
+    
+    def mul(self, son1, mom1, son2, mom2):
+        new_fen = [son1*son2, mom1*mom2]
+        return yue(new_fen[0], new_fen[1])
+    
+    def div(self, son1, mom1, son2, mom2):
+        new_fen = [son1*mom2, mom1*son2]
+        return yue(new_fen[0], new_fen[1])
+    
+        
 
 #使用
 #输入
@@ -81,30 +73,109 @@ for i in range(time):
     s.append(int(input(f"请输入第{i+1}个分数的分子：")))
     m.append(int(input(f"请输入第{i+1}个分数的分母：")))
     print(f"第{i+1}个分数：{y[i]} {s[i]}/{m[i]}\n")
-#需求
-what = input("请输入需要进行的运算（加1、减2、乘3、除4、退出0）：")
-    
-k = know()
-if what == "1":
-    result = k.add(s, m)
-    print(f"结果：{result[0]}/{result[1]}\n")
-elif what == "2":
-    result = k.sub(s, m)
-    print(f"结果：{result[0]}/{result[1]}\n")
-elif what == "3":
-    result = k.mul(s, m)
-    print(f"结果：{result[0]}/{result[1]}\n")
-elif what == "4":
-    result = k.div(s, m)
-    print(f"结果：{result[0]}/{result[1]}\n")
-elif what == "0":
-    print("退出程序。\n")
-    exit()
+#输出列表
+#每个列表每个元素都是一个中间的结果，如：1/2 + 1/2 + 1/2，则这个3个列表为:[1,1][0,1][0,2]
+zn = []
+sn = []
+mn = []
+
+#计算与推进
+if time == 1:
+    # 如果只有一个分数，直接输出
+    zn.append(y[0])
+    sn.append(s[0])
+    mn.append(m[0])
 else:
-    print("无效的输入，请重新输入。\n")
-    
+    for i in range(time-1):
+        what = input(f"请输入第{i+1}个符号（加1、减2、乘3、除4、退出0）：")
+
+        if what == '0':
+            print("退出程序！")
+            break
+
+        #这里因为列表无法修改元素的同时删除元素，所以只能新建一个列表来存储每步运算的结果
+        '''
+        计算步骤
+        1.先把整数部分转化为分数部分
+        2.通分
+        3.计算
+        4.约分（如果分子>分母则转化为带分数）
+        '''
+
+        #1
+        for j in range(time):
+            s[j] += y[j]*m[j]
+            y[j] = 0
         
-       
+        #2-4
+        if i == 0:
+            if what == '1':
+                now_fen = tong(s[i], m[i], s[i+1], m[i+1])
+                new_fen = [now_fen[0]+now_fen[1], now_fen[2]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '2':
+                now_fen = tong(s[i], m[i], s[i+1], m[i+1])
+                new_fen = [now_fen[0]-now_fen[1], now_fen[2]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '3':
+                new_fen = [s[i]*s[i+1], m[i]*m[i+1]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '4':
+                new_fen = [s[i]*m[i+1], m[i]*s[i+1]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            else:
+                print("输入错误，请重新输入！")
+        else:
+            current_son = zn[i-1] * mn[i-1] + sn[i-1]
+            if what == '1':
+                now_fen = tong(current_son, mn[i-1], s[i+1], m[i+1])
+                new_fen = [now_fen[0]+now_fen[1], now_fen[2]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '2':
+                now_fen = tong(current_son, mn[i-1], s[i+1], m[i+1])
+                new_fen = [now_fen[0]-now_fen[1], now_fen[2]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '3':
+                new_fen = [current_son * s[i+1], mn[i-1] * m[i+1]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            elif what == '4':
+                new_fen = [current_son * m[i+1], mn[i-1] * s[i+1]]
+                new_fen = yue(new_fen[0], new_fen[1])
+                zn.append(int(new_fen[0]//new_fen[1]))
+                sn.append(int(new_fen[0]%new_fen[1]))
+                mn.append(int(new_fen[1]))
+            else:
+                print("输入错误，请重新输入！")
+            
+
+        
+
+
+
+
+#输出结果
+print(f"结果：{zn[-1]} {sn[-1]}/{mn[-1]}")
 
 
 
